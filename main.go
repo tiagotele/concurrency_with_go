589b8d0a2c29acfbc56ee757059af46b3e993208
Tiago Melo <tiagotele@gmail.com>
2018-03-12 06:23:03 -0300

    Adding channel approach

--- main.go
+++ main.go
@@ -7,7 +7,32 @@ import (
 )
 
 func main() {
+	using_channel()
+}
 
+func using_channel(){
+	number := flag.Int("number", 10, "Number as parameter")
+	flag.Parse()
+	fmt.Println("The argument is", *number)
+
+	messages := make(chan int)
+
+	for i := 0; i < *number; i++ {
+		go task_with_channel(i, messages)
+	}
+
+	for i := 0; i < *number; i++ {
+        (<-messages)
+    }
+	fmt.Println("Done")
+}
+
+func task_with_channel(taskNumber int, messages chan<- int){
+	print_task_number(taskNumber)
+	messages <- taskNumber
+}
+
+func using_sync(){
 	number := flag.Int("number", 10, "Number as parameter")
 	flag.Parse()
 	fmt.Println("The argument is", *number)
@@ -16,16 +41,18 @@ func main() {
 	wg.Add(*number)
 
 	for i := 0; i < *number; i++ {
-		go task(i, &wg)
+		go task_with_sync(i, &wg)
 	}
 
 	wg.Wait()
 	fmt.Println("Done")
 }
 
-func task(taskNumber int, wg *sync.WaitGroup) {
-
+func task_with_sync(taskNumber int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Println(fmt.Sprintf("Task number {%d}", taskNumber))
+	print_task_number(taskNumber)
+}
 
+func print_task_number(taskNumber int) {
+	fmt.Println(fmt.Sprintf("Task number {%d}", taskNumber))
 }
