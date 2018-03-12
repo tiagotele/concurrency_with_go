b556f6c24546b9ff4484865d42b2896e79d5ca0c
Tiago Melo <tiagotele@gmail.com>
2018-03-12 06:10:12 -0300

    Sync all go routines

--- main.go
+++ main.go
@@ -1,25 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"sync"
 )
 
-func main(){
-	
-	number := flag.Int("number", 10, "Number as parameter")
+func main() {
 
+	number := flag.Int("number", 10, "Number as parameter")
 	flag.Parse()
 	fmt.Println("The argument is", *number)
-	
-	for i := 0 ; i < *number ; i++{
-		go task(i)
+
+	var wg sync.WaitGroup
+	wg.Add(*number)
+
+	for i := 0; i < *number; i++ {
+		go task(i, &wg)
 	}
 
-	fmt.Scanln()
-    fmt.Println("Done")
+	wg.Wait()
+	fmt.Println("Done")
 }
 
-func task(taskNumber int){
+func task(taskNumber int, wg *sync.WaitGroup) {
+
+	defer wg.Done()
 	fmt.Println(fmt.Sprintf("Task number {%d}", taskNumber))
+
 }
