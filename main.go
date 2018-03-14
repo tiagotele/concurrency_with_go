e7374489ebc70488481a4e6d533dae87bff22903
Tiago Melo <tiagotele@gmail.com>
2018-03-14 00:50:11 -0300

    Initial tasks. More work need be done here

--- main.go
+++ main.go
@@ -7,7 +7,28 @@ import (
 )
 
 func main() {
-	using_channel()
+	number := flag.Int("number", 10, "Number as parameter")
+	tn := flag.Int("tn", 3, "Number as parameter")
+
+	flag.Parse()
+
+	fmt.Println("Values ", *number, " ",*tn)
+	if *tn > *number{
+		panic("tn can`t be greater than number")
+	}
+
+
+	for indexNumber := 0 ; indexNumber < *number ; indexNumber = indexNumber + (*tn){
+		for tnNumber := 0 ; tnNumber<*tn; tnNumber++{
+			fmt.Println(fmt.Sprintf("Number %d, value %d",indexNumber, indexNumber + tnNumber))
+			if *number - indexNumber < *tn{
+				tnNumber = *number - indexNumber
+				break
+			}
+			
+		}
+
+	}
 }
 
 func using_channel(){
